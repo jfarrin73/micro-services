de1a05d274edcde108f84841123f1c230ecf6edd
Josh <joshua.l.farrington.civ@army.mil>
2023-09-19 13:52:38 -0500

    use OpenFeign to communicate between services

--- employee-service/src/main/java/com/example/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/example/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.employeeservice.dto.APIResponseDto;
 import com.example.employeeservice.dto.DepartmentDto;
 import com.example.employeeservice.dto.EmployeeDto;
 import com.example.employeeservice.repository.EmployeeRepository;
+import com.example.employeeservice.service.APIClient;
 import com.example.employeeservice.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     private EmployeeRepository employeeRepository;
     private RestTemplate restTemplate;
     private WebClient webClient;
+    private APIClient apiClient;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -39,15 +41,19 @@ public class EmployeeServiceImpl implements EmployeeService {
     public APIResponseDto getEmployeeById(Long id) {
         Employee employee = employeeRepository.findById(id).get();
 
-        String url = "http://localhost:8080/api/departments/" + employee.getDepartmentCode();
+//        String url = "http://localhost:8080/api/departments/" + employee.getDepartmentCode();
+
 //        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity(url, DepartmentDto.class);
 //        DepartmentDto departmentDto = responseEntity.getBody();
-        DepartmentDto departmentDto = webClient
-                .get()
-                .uri(url)
-                .retrieve()
-                .bodyToMono(DepartmentDto.class)
-                .block();
+
+//        DepartmentDto departmentDto = webClient
+//                .get()
+//                .uri(url)
+//                .retrieve()
+//                .bodyToMono(DepartmentDto.class)
+//                .block();
+
+        DepartmentDto departmentDto = apiClient.getDepartment(employee.getDepartmentCode());
 
         EmployeeDto employeeDto = new EmployeeDto(
                 employee.getId(),
